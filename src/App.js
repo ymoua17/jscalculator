03d8e8e884a0865fcb3aed454cb6945445dcbf69
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-03-31 12:19:12 -0500

    added functionallity

--- src/App.js
+++ src/App.js
@@ -5,43 +5,57 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value : 0,
+      output: '',
     };
   }
+  handleClearAll = () => {
+    this.setState({
+      output: '',
+    })
+  }
+  handleButton = (event) => {
+    const arrayOutput = this.state.output.concat(event.target.value);
+    this.setState({
+      output: arrayOutput,
+    })
+  }
+
+  
   render() {
+    const displayOutput = this.state.output;
+    console.log(displayOutput);
     return (
       <div className="App">
         <h3>Javascript Calculator</h3>
         <h5>**Created by ymoua17**</h5>
         <div id='display'>
-          {this.state.value}
+          {displayOutput}
         </div>
         <div className="inputs">
-          <button id="clear" className="btn btn-danger">A/C</button>
-          <button id="add" className="btn btn-light">+/-</button>
-          <button id="percent" className="btn btn-light">%</button>
-          <button id="divide" className="btn btn-light">/</button>
+          <button id="clear" className="btn btn-danger" onClick={this.handleClearAll}>A/C</button>
+          <button id="add" className="btn btn-light" value="" onClick={this.handleButton}>+/-</button>
+          <button id="percent" className="btn btn-light" value="" onClick={this.handleButton}>%</button>
+          <button id="divide" className="btn btn-light" value="/" onClick={this.handleButton}>/</button>
           <br/>
-          <button id="nine" className="btn btn-secondary">9</button>
-          <button id="eight" className="btn btn-secondary">8</button>
-          <button id="seven" className="btn btn-secondary">7</button>
-          <button id="multiply" className="btn btn-light">*</button>
+          <button id="nine" className="btn btn-secondary" value="9" onClick={this.handleButton}>9</button>
+          <button id="eight" className="btn btn-secondary" value="8" onClick={this.handleButton}>8</button>
+          <button id="seven" className="btn btn-secondary" value="7" onClick={this.handleButton}>7</button>
+          <button id="multiply" className="btn btn-light" value="*" onClick={this.handleButton}>*</button>
           <br/>
-          <button id="six" className="btn btn-secondary">6</button>
-          <button id="five" className="btn btn-secondary">5</button>
-          <button id="four" className="btn btn-secondary">4</button>
-          <button id="subtract" className="btn btn-light">-</button>
+          <button id="six" className="btn btn-secondary" value="6" onClick={this.handleButton}>6</button>
+          <button id="five" className="btn btn-secondary" value="5" onClick={this.handleButton}>5</button>
+          <button id="four" className="btn btn-secondary" value="4" onClick={this.handleButton}>4</button>
+          <button id="subtract" className="btn btn-light" value="-" onClick={this.handleButton}>-</button>
           <br/>
-          <button id="three" className="btn btn-secondary">3</button>
-          <button id="two" className="btn btn-secondary">2</button>
-          <button id="one" className="btn btn-secondary">1</button>
-          <button id="add" className="btn btn-light">+</button>
+          <button id="three" className="btn btn-secondary" value="3" onClick={this.handleButton}>3</button>
+          <button id="two" className="btn btn-secondary" value="2" onClick={this.handleButton}>2</button>
+          <button id="one" className="btn btn-secondary" value="1" onClick={this.handleButton}>1</button>
+          <button id="add" className="btn btn-light" value="+" onClick={this.handleButton}>+</button>
           <br/>
-          <button id="zero" className="btn btn-secondary lg-btn">0</button>
-          <button id="decimal" className="btn btn-secondary">.</button>
+          <button id="zero" className="btn btn-secondary lg-btn" value="0" onClick={this.handleButton}>0</button>
+          <button id="decimal" className="btn btn-secondary" value="." onClick={this.handleButton}>.</button>
           <button id="equal" className="btn btn-success">=</button>
         </div>
-        
       </div>
     );
   }
