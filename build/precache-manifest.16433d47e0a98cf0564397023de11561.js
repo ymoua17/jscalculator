3f616e4fe8bb0cdfb0a4a74994f03b908ab20412
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2020-04-21 19:56:28 -0500

    update

---
+++ build/precache-manifest.16433d47e0a98cf0564397023de11561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b3bfc54df0c092e366b107ab7dfd4",
+    "url": "/jscalculator/index.html"
+  },
+  {
+    "revision": "7a94f7802d880012e4b5",
+    "url": "/jscalculator/static/css/main.a9318754.chunk.css"
+  },
+  {
+    "revision": "4086bd8da46303c4fefe",
+    "url": "/jscalculator/static/js/2.3fd566d2.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/jscalculator/static/js/2.3fd566d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a94f7802d880012e4b5",
+    "url": "/jscalculator/static/js/main.fc92fe6c.chunk.js"
+  },
+  {
+    "revision": "1888fd1582e9c198f611",
+    "url": "/jscalculator/static/js/runtime-main.a135195f.js"
+  }
+]);
